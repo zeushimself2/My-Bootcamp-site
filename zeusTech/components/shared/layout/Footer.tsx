2c09c8040893e8c1a2898ccea1acf8c80632777b
Fordjour Bright <fordjourfordjourbright@gmail.com>
2025-07-03 17:09:33 +0000

    Update Footer.tsx

--- zeusTech/components/shared/layout/Footer.tsx
+++ zeusTech/components/shared/layout/Footer.tsx
@@ -19,7 +19,7 @@ export const Footer = () => {
         </p>
   
         <Link
-          href="/register"
+          href="/checkout/[id]"
           className="mt-8 inline-block rounded-full border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500"
         >
           Register
